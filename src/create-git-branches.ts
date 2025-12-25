8584712b602f98e7f66e05d2be8120bc2200db14
Victor Musienko <8777372+sdwvit@users.noreply.github.com>
2025-12-25 21:26:58 +0000

    Update Git push command to set upstream branch

--- src/create-git-branches.ts
+++ src/create-git-branches.ts
@@ -12,7 +12,7 @@ const cmd = (c: string) =>
   });
 
 validMods.forEach((mod) => {
-  cmd(["git", "checkout", "-b", mod].join(" "));
+  cmd(["git", "checkout", mod].join(" "));
   const envFile = path.join(projectRoot, ".env.modname");
   fs.writeFileSync(envFile, `MOD_NAME=${modName}\n`);
   cmd(["git", "add", envFile].join(" "));
