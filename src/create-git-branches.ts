0f1b6e89631b923ade4977c25be0989ffb1f7bd3
Victor Musienko <8777372+sdwvit@users.noreply.github.com>
2025-12-25 21:35:50 +0000

    Update Git push command to set upstream branch

--- src/create-git-branches.ts
+++ src/create-git-branches.ts
@@ -1,23 +1,27 @@
-import { modName, projectRoot, validMods } from "./base-paths.mts";
+import { projectRoot, validMods } from "./base-paths.mts";
 import { spawnSync } from "child_process";
 import fs from "node:fs";
 import path from "node:path";
+import { logger } from "./logger.mts";
 
-const cmd = (c: string) =>
-  spawnSync(c, {
+const cmd = (c: string) => {
+  logger.log("-- Executing command", c);
+  return spawnSync(c, {
     stdio: "inherit",
     cwd: import.meta.dirname,
     shell: "/usr/bin/bash",
     env: { ...process.env },
   });
+};
 
 validMods.forEach((mod) => {
+  cmd(["git", "checkout", "master"].join(" "));
   cmd(["git", "checkout", "-b", mod].join(" "));
   const envFile = path.join(projectRoot, ".env.modname");
-  fs.writeFileSync(envFile, `MOD_NAME=${modName}\n`);
+  fs.writeFileSync(envFile, `MOD_NAME=${mod}\n`);
   cmd(["git", "add", envFile].join(" "));
   cmd(["git", "commit", "-m", "'Add new mod'"].join(" "));
-  cmd(["git", "push", "origin", modName].join(" "));
+  cmd(["git", "push", "--set-upstream", "origin", mod].join(" "));
   cmd(["git", "checkout", "master"].join(" "));
 });
 
